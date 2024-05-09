32939ccd11dafd5ca360d4c89cae3b09ef32a159
chulipinho <fellipe.luz.machado@gmail.com>
2024-05-09 08:59:09 -0300

    Removes temporary files after execution

--- main.py
+++ main.py
@@ -5,18 +5,24 @@ from drive import Drive
 import os
 from dotenv import load_dotenv
 
-# todo: add log, remove files from temp folder, convert response to json, send email
+# todo: add log, convert response to json, send email
 
-# runs only if file is main
+load_dotenv()
+CLEAR_TMP = os.getenv("CLEAR_TMP", True)
+VIDEO_PATH = os.getenv("VIDEO_PATH", "meetings")
+
+def clear_tmp():
+    files = os.listdir("tmp")
+
+    for f in files:
+        os.remove("tmp/" + f)
 
 def app():
-    load_dotenv()
-    path = os.getenv("VIDEO_PATH")
+    path = VIDEO_PATH
 
     drive = Drive()
     drive.authorize()
 
-    
     files = drive.get_content(path=path, mime_type="video")
     
     for f in files:
@@ -27,6 +33,8 @@ def app():
         res = analize_audio(audio_path)
         
         print(res)
+
+    if CLEAR_TMP: clear_tmp()
     
 
 if __name__ == "__main__":
