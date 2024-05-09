8cceb50421693fba62e3672e28b99a4e963f354f
chulipinho <fellipe.luz.machado@gmail.com>
2024-05-09 07:49:50 -0300

    Clear unused variables

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from drive import Drive
 
 import os
 
-# todo: add log, remove files from temp folder, refactor drive logic, convert response to json, send email
+# todo: add log, remove files from temp folder, convert response to json, send email
 
 # runs only if file is main
 if __name__ == "__main__":
@@ -14,8 +14,6 @@ if __name__ == "__main__":
     path = os.getenv("VIDEO_PATH")
     files = drive.get_content(path=path, mime_type="video")
     
-    prompt = "This is a meeting recording. Create a bullet summary of this meeting extracting its main points and write a brief conclusion."
-
     for f in files:
         path = "tmp/" + f["title"]
         f.GetContentFile(path)
